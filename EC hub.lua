60d33e4059fbd9db29f838a5bdfe596cd06bc5b4
shadowquantQ <viskasviskovich778@gmail.com>
2024-07-22 11:39:25 +0300

    Create EC hub.lua

---
+++ EC hub.lua
@@ -0,0 +1,7 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Shadow hub - Empire clash", "RJTheme3")
+
+local Tab = Window:NewTab("Main menu")
+
+local Section = Tab:NewSection("Main menu")
